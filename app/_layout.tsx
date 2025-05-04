d578d6e422403a75cdd18aabb00f9cec13dfa243
Tech-Maicha-01 <153009970+Tech-Maicha-01@users.noreply.github.com>
2025-05-04 16:38:00 +0545

    May 4th

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,26 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
+import "./globals.css";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
     return (
-        <Stack
-            screenOptions={{
-                headerShown: false, // 👈 hide stack header globally
-            }}
-        />
+        <>
+            <StatusBar hidden={true} />
+
+            <Stack>
+                <Stack.Screen
+                    name="(tabs)"
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+                <Stack.Screen
+                    name="movie/[id]"
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+            </Stack>
+        </>
     );
 }
